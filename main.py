568ffb661bf2a709bcb0eb8860d0f938c9651194
labelq <113503114+labelq@users.noreply.github.com>
2023-06-09 22:40:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 from tinydb import TinyDB, Query
 
 
-bot = telebot.TeleBot('6236893434:AAEQO3bukIPCHkhhtqdjHDqYgFkR1XLJ5Pw')
+bot = telebot.TeleBot('5045463995:AAF9swJxTOlj7l2y7wZuNaaJ7zU1YGz92FQ')
 
 user_data = {}
 db = TinyDB('answers_db.json')
@@ -24,38 +24,42 @@ def gen_markup(options, k):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     req = call.data.split('_')
-    print(req)
     json_string = json.loads(req[0])
     k = json_string['questionNumber'] + 1
     answer = json_string['answerText']
-    if k == 0 and answer == "Нет":
-        k = -1
+    
+    if k == 0 and answer == "Нет, я боюся":
         return bot.edit_message_text(chat_id=call.message.chat.id,
                                      message_id=call.message.message_id,
-                                     text='Ну и ладно ')
+                                     text='Ну и бойся дальше:(((\nНо если передумаешь, нажми /start')
     user_id = call.from_user.id
     if user_id not in user_data:
         user_data[user_id] = Anket(questions)
     anket = user_data[user_id]
-    anket.answers.append(json_string)
+    
+    if k != 0:  # Исключаем запись ответа "Погнали"
+        anket.answers.append(json_string)
+    
+    if k == -1:
+        return
+    
     if k == anket.length:
         score = anket.add_answers(anket.answers)
         del user_data[user_id]
 
         # Сохранение результатов в базу данных
         db.insert({'user_id': user_id, 'score': score, 'answers': anket.answers})
-        print('Баллы: ',score)
 
         return bot.edit_message_text(chat_id=call.message.chat.id,
                                      message_id=call.message.message_id,
-                                     text=f'Спасибо за уделённое время! \nЧтобы посмотреть свои результаты нажмите /results')
+                                     text=f'Спасибо за уделённое время, вы набрали: {score} из {anket.length} баллов. \nБолее подробно результаты вы можете посмотреть через команду /results')
 
     button_column = anket.config[k].get('options')
     bot.edit_message_text(chat_id=call.message.chat.id,
                           message_id=call.message.message_id,
                           text=anket.get_question(k),
                           reply_markup=gen_markup(button_column, k))
-
+    
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
@@ -65,9 +69,10 @@ def start(message):
     anket.answers = []
     db.remove(Query().user_id == user_id)  # Удаление предыдущих ответов пользователя из базы данных
     k = -1
-    button_column = ['Да', 'Нет']
-    bot.send_message(chat_id=message.chat.id, text="Привет! \nСможешь пройти мой опрос?",
+    button_column = ['Погнали', 'Нет, я боюся']
+    bot.send_message(chat_id=message.chat.id, text="Привет, я бот-анкета! \nОтветь на мои вопросы?",
                      reply_markup=gen_markup(button_column, k))
+
 
 @bot.message_handler(commands=['results'])
 def show_results(message):
@@ -91,5 +96,6 @@ def show_results(message):
         response = "У вас пока нет результатов опроса."
 
     bot.send_message(chat_id=message.chat.id, text=response)
+
 
 bot.polling()
